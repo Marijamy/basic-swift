bffbb5bd1b3fea07c0844b2ae683013f08aa2eb7
Aksenova <marijamy777@gmail.com>
2021-03-15 13:47:16 +0300

    lesson7

---
+++ L7_1_AksenovaMary.playground/Contents.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+enum CandyBar: Error{
+    case lostLot
+    case accesError
+    case noLot
+}
+
+struct Product{
+    var brand: String
+    var count: Int
+}
+
+class SweetBox{
+    
+    var Sweets = [
+        "Mars" : Product(brand: "Mars", count: 3),
+        "Oreo" : Product(brand: "Oreo", count: 0),
+        "BonAqua" : Product(brand: "BonAqua", count: 5)
+    ]
+    
+    var countMin = 0
+    var errorValue = 100
+    
+    func sweetBay(itemBrand name:String, error: Int) throws {
+       
+        guard let lot = Sweets[name] else {
+            throw CandyBar.noLot
+        }
+        
+        guard lot.count > countMin else {
+            throw CandyBar.lostLot
+        }
+        
+        guard error == errorValue else{
+            throw CandyBar.accesError
+        }
+        
+        print("Спасибо за покупку!")
+    }
+}
+
+
+let collectionSweets = [
+    "Chocolate" : "Mars",
+    "Biscuit" : "Oreo",
+    "Water" : "AquaMimerale"
+]
+
+func bay(collection: String, funcToBay: SweetBox) throws {
+    
+    let serverMessage = 100
+    let collectionBar = collectionSweets[collection] ?? "Выберете товар"
+    
+    try funcToBay.sweetBay(itemBrand: collectionBar, error: serverMessage)
+}
+
+
+do {
+    try bay(collection: "Water", funcToBay: SweetBox())
+} catch CandyBar.noLot {
+    print("Такой продукт отсутствует в ассортименте.")
+} catch CandyBar.lostLot {
+    print("Данный товар закончился. Приносим изинения.")
+} catch CandyBar.accesError {
+    print("Автомат временно не наботает.")
+}
