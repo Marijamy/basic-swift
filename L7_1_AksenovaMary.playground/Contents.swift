40bc7a5c63d4725546eb6fc3477419fc5c077af2
Aksenova <marijamy777@gmail.com>
2021-03-15 14:40:39 +0300

    lesson 7 update

--- L7_1_AksenovaMary.playground/Contents.swift
+++ L7_1_AksenovaMary.playground/Contents.swift
@@ -4,25 +4,27 @@ enum CandyBar: Error{
     case lostLot
     case accesError
     case noLot
+    case hightPrice
 }
 
 struct Product{
     var brand: String
     var count: Int
+    var price: Int
 }
 
 class SweetBox{
     
     var Sweets = [
-        "Mars" : Product(brand: "Mars", count: 3),
-        "Oreo" : Product(brand: "Oreo", count: 0),
-        "BonAqua" : Product(brand: "BonAqua", count: 5)
+        "Mars" : Product(brand: "Mars", count: 3, price: 30),
+        "Oreo" : Product(brand: "Oreo", count: 0, price: 20),
+        "BonAqua" : Product(brand: "BonAqua", count: 5, price: 25)
     ]
     
     var countMin = 0
     var errorValue = 100
     
-    func sweetBay(itemBrand name:String, error: Int) throws {
+    func sweetBay(itemBrand name:String, deposit: Int, error: Int) throws {
        
         guard let lot = Sweets[name] else {
             throw CandyBar.noLot
@@ -36,6 +38,10 @@ class SweetBox{
             throw CandyBar.accesError
         }
         
+        guard deposit == lot.price else{
+            throw CandyBar.hightPrice
+        }
+        
         print("Спасибо за покупку!")
     }
 }
@@ -47,21 +53,23 @@ let collectionSweets = [
     "Water" : "AquaMimerale"
 ]
 
-func bay(collection: String, funcToBay: SweetBox) throws {
+func bay(collection: String, deposit: Int, funcToBay: SweetBox) throws {
     
     let serverMessage = 100
     let collectionBar = collectionSweets[collection] ?? "Выберете товар"
     
-    try funcToBay.sweetBay(itemBrand: collectionBar, error: serverMessage)
+    try funcToBay.sweetBay(itemBrand: collectionBar,deposit: deposit, error: serverMessage)
 }
 
 
 do {
-    try bay(collection: "Water", funcToBay: SweetBox())
+    try bay(collection: "Chocolate", deposit: 25, funcToBay: SweetBox())
 } catch CandyBar.noLot {
     print("Такой продукт отсутствует в ассортименте.")
 } catch CandyBar.lostLot {
     print("Данный товар закончился. Приносим изинения.")
 } catch CandyBar.accesError {
     print("Автомат временно не наботает.")
+} catch CandyBar.hightPrice {
+    print("Внесите средства.")
 }
